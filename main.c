43a9fd4e51179413847b1ffa93005dcf6265cdbe
Jefer <jefer@asd>
2014-04-22 09:09:10 -0300

    menu

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
 int main(){
+    int op, a, b;
+
+    printf("Digite a opção: ");
+    scanf("%d", &op);
+
+    printf("Digite um numero: ");
+    scanf("%d", &a);
+    printf("Digite um numero: ");
+    scanf("%d", &b);
+
+    switch(op){
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+    }
 
     return 0;
 }
