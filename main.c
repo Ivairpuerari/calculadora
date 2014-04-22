12a98778c5ba3b3d15f84eb8be52042acf34450d
Jefer <jefer@asd>
2014-04-22 09:32:20 -0300

    função primos entre

--- main.c
+++ main.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 
 
-void testaPrimos(int a, int b)
+void testaPrimos(int a, int b){
+    int primo, i, j, num;
+
+    if(a >= b){
+        num = a-b;
+        int v[num];
+        j = a;
+        for(i = 0; j <= b; i++){
+            v[i] = j;
+            j++;
+        }
+    }else{
+        num = b - a;
+        int v[num];
+        j = b;
+        for(i = 0; j <= a; i++){
+            v[i] = j;
+            j++;
+        }
+    }
+    i = 0;
+    int primos[num];
+    for(j = v[0]; j <= num; j++){
+        if(v[j] % 1 == 0 && v[j] % 2 == 0 || v[j] % 3 == 0){
+            primos[i] = v[j];
+            i++;
+        }
+    }
+
+    for(i = 0; i <= num; i++){
+        printf("Primo: %d\n", v[i]);
+    }
+
+
+}
 
 int main(){
     int op, a, b;
@@ -20,6 +54,7 @@ int main(){
         case 1:
             break;
         case 2:
+            testaPrimos(a,b);
             break;
         case 3:
             break;
